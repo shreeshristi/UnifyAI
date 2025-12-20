e111e22f083a8541d8c03028bcf89cc53fbd1982
Shristi Shree <2023ugee048@nitjsr.ac.in>
2025-12-20 12:49:32 +0530

    image api

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
 // open ai configuration
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -12,7 +13,7 @@ export const generateArticle = async (req, res) => {
   try {
     // inputs ie auth and prompt plan
     const { userId } = req.auth(); 
-    const { prompt, length } = req.body; 
+    const { prompt,length } = req.body; 
     const plan = req.plan ;
     const free_usage = req.free_usage;
 
@@ -26,23 +27,115 @@ export const generateArticle = async (req, res) => {
     }
 // pass prompt and get response
     const response = await AI.chat.completions.create({
-      model: "gemini-2.0-flash",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
+      model: "gemini-2.5-flash",
+      messages: [{role: "user", content: prompt,},],
       temperature: 0.7,
       max_tokens: length,
     });
+    const content = response.choices[0].message.content
 
-    const content = response.choices[0].message.content;
+
 // insert in database neon DB INSERTION
     await sql`
       INSERT INTO creations (user_id, prompt, content, type)
       VALUES (${userId}, ${prompt}, ${content}, 'article')`
     ;
+// 1 free trial used CLERK UPDATE
+    if (plan !== 'premium') {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1
+        }
+      })
+    }
+    res.json({ success: true, content })
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+
+
+export const generateBlogTitle = async (req, res) => {
+  try {
+    // inputs ie auth and prompt plan
+    const { userId } = req.auth(); 
+    const { prompt } = req.body; 
+    const plan = req.plan ;
+    const free_usage = req.free_usage;
+
+
+    // free use exhausted then dont generate API CALL
+    if (plan !== 'premium' && free_usage >= 10) {
+      return res.json({
+        success: false,
+        message: "Limit reached. Upgrade to continue."
+      });
+    }
+// pass prompt and get response
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.5-flash",
+      messages: [{role: "user", content: prompt,}],
+      temperature: 0.7,
+      max_tokens: length,
+    });
+    const content = response.choices[0].message.content
+
+    
+// insert in database neon DB INSERTION
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type)
+      VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+// 1 free trial used CLERK UPDATE
+    if (plan !== 'premium') {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1
+        }
+      })
+    }
+    res.json({ success: true, content })
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+
+export const generateImage = async (req, res) => {
+  try {
+    // inputs ie auth and prompt plan
+    const { userId } = req.auth(); 
+    const { prompt, publish } = req.body; 
+    const plan = req.plan ;
+    // const free_usage = req.free_usage;
+
+
+    // free use exhausted then dont generate API CALL
+    if (plan !== 'premium' ) {
+      return res.json({ success: false, message: "This feature is only for premium users"});
+    }
+// pass prompt and get response
+    const formData = new FormData()
+    formData.append('prompt', prompt)
+    const {data}= await axios.post("https://clipdrop-api.co/text-to-image/v1", formData,{
+        headers: {'x-api-key': process.env.CLIPDROP_API_KEY},
+        responseType: "arraybuffer"
+    })
+
+    const base64image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+    
+// insert in database neon DB INSERTION
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type)
+      VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+      
 // 1 free trial used CLERK UPDATE
     if (plan !== 'premium') {
       await clerkClient.users.updateUserMetadata(userId, {
